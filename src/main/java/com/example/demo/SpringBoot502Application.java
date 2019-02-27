d67ca116a5dbd19e6c65d91e19c3258c58a49c09
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-27 11:38:15 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot502Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot502Application.class, args);
+    }
+
+}
